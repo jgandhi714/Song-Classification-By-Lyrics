6f6de846d76aae32f01f96de5983daee537dbc79
Javesh Gandhi <javesh.gandhi@gmail.com>
2016-12-28 15:51:15 -0500

    Fixed page skipping

--- Genius_Scraper.py
+++ Genius_Scraper.py
@@ -18,6 +18,7 @@ class Genius_Scraper:
         self.artist = artist
         self.albums = [album.lower() for album in albums]
         self.artist_api_path = self.get_artist_api_path()
+        self.directory = "artists/%s" % self.artist
     
     def strip(self, s):
         s = s.replace('?', '')
@@ -50,36 +51,7 @@ class Genius_Scraper:
                 return artist_name["api_path"]
             else:
                 return None
-    """
-    def get_song_api_paths(self):
-        artist_api_path = self.get_artist_api_path()
-        song_api_paths = []
-        artist_songs_url = BASE_URL + artist_api_path + "/songs"
-        #iterate through as many pages as possible - once an error is hit, break from the loop        
-        for i in range(1, 51):
-            #try:
-            data = {"page": i}
-            response = requests.get(artist_songs_url, data=data, headers=headers)
-            artist_songs_info = response.json()
-            songs = artist_songs_info["response"]["songs"]
-            for song in songs:
-                song_api_path = song["api_path"]
-                if song['primary_artist']['api_path'] == artist_api_path:
-                    try:
-                        if self.get_song_info(song_api_path)["response"]["song"]["album"]["name"].lower() in self.albums:
-                            print("Appending " + get_song_title(song_api_path))                            
-                            song_api_paths.append(song_api_path)
-                        else:
-                            print(self.get_song_title(song_api_path) + " is not in any of the given albums!")
-                            continue
-                    except (TypeError, ValueError):
-                        print(self.get_song_title(song_api_path) + " album could not be found!")
-                else:
-                    print(self.artist + " is not the primary artist for " + self.get_song_title(song_api_path))
-            i +=1
-        #except()
-        return list(set(song_api_paths))
-        """
+                
     def get_song_info(self, song_api_path):
         song_url = BASE_URL + song_api_path
         response = requests.get(song_url, headers=headers)
@@ -134,111 +106,7 @@ class Genius_Scraper:
         #lyrics = lyrics.replace("\n", "")
         #lyrics = lyrics.replace("\r", "")
         return lyrics
-    """
-    def create_txt(self):
-        #set up directories to store txt files
-        artist_folder_path = "artists/%s" % self.artist
-        if not os.path.exists(artist_folder_path):
-            os.makedirs(artist_folder_path)
-        
-        blank_folder_path = artist_folder_path + '/blank'
-        if not os.path.exists(blank_folder_path):
-            os.makedirs(blank_folder_path)
-        
-        artist_api_path = self.artist_api_path
-        artist_songs_url = BASE_URL + artist_api_path + "/songs"
-        #iterate through as many pages as possible - once an error is hit, break from the loop        
-               
-        for i in range(1, 101):
-            j = int(i)            
-            rts_i = str(j)
-            #print("ON PAGE " + rts_i)
-            try:
-                
-                data = {"page": i}
-                response = requests.get(artist_songs_url, data=data, headers=headers)
-                try:                
-                    artist_songs_info = response.json()
-                except(IndexError):
-                    print ("ALL DONE!!!")
-                    break
-                songs = artist_songs_info["response"]["songs"]
-                
-                #check to see if we already have the text files from this page and skip it if so in order to let us
-                #start at the page we left off (in case of connection break from earlier download session)
-                try:                
-                    check_song = songs[-1]
-                    print("ON PAGE " + rts_i)
-                except(IndexError):
-                    print ("ALL DONE!!!")                    
-                    break
-                
-                check_song_api_path = check_song["api_path"]
-                check_song_title = self.get_song_title(check_song_api_path)
-                check_lyric_path = artist_folder_path + '/%s' % self.strip(check_song_title) + '.txt'
-                check_blank_path = blank_folder_path + '/%s' % self.strip(check_song_title) + 'txt'
-                if os.path.exists(check_lyric_path) or os.path.exists(check_blank_path):
-                    continue
-                for song in songs:
-                    song_api_path = song["api_path"]
-                    song_title = self.get_song_title(song_api_path)
-                    lyric_path = artist_folder_path + '/%s' % self.strip(song_title) + '.txt'
-                    blank_path = artist_folder_path + '/Blank/' + '/%s' % self.strip(check_song_title) + 'txt'
-                    
-                    #check if primary artist is the right one`
-                    if song['primary_artist']['api_path'] == artist_api_path:
-                        try:
-                            #check if song is in list of albums
-                            if self.get_song_info(song_api_path)["response"]["song"]["album"]["name"].lower() in self.albums:
-                                #check if already downloaded                                
-                                if not os.path.exists(lyric_path):
-                                    print(".......CREATING TXT FILE " + song_title)
-                                    lyrics = self.clean_lyrics(song_api_path)                                    
-                                    with open(lyric_path, "w") as ifile:
-                                        try:
-                                            ifile.write(lyrics)
-                                        except UnicodeEncodeError as e:
-                                            print(e)
-                                else:
-                                    if os.path.getsize(lyric_path) == 0:
-                                        print(".......CREATING TXT FILE " + song_title)
-                                        lyrics = self.clean_lyrics(song_api_path)                                    
-                                        with open(lyric_path, "w") as ifile:
-                                            try:
-                                                ifile.write(lyrics)
-                                            except UnicodeEncodeError as e:
-                                                print(e)
-                                    else:
-                                        print("TXT FILE ALREADY EXISTS FOR " + song_title)
-                            else:
-                                 with open(blank_path, "w") as ifile:
-                                     try:
-                                         ifile.write()
-                                     except UnicodeEncodeError as e:
-                                         print(e)                                
-                                #print(song_title + " is not in any of the given albums!")
-                                 continue
-                        except (TypeError, ValueError):
-                            #print(self.get_song_title(song_api_path) + " album could not be found!")
-                            continue
-#==============================================================================
-#                     else:
-#                         print(self.artist + " is not the primary artist for " + self.get_song_title(song_api_path))   
-#==============================================================================
-            #i = i + 1
-                else:
-                    with open(blank_path, "w") as ifile:
-                        try:
-                            ifile.write()
-                        except UnicodeEncodeError as e:
-                            print(e)  
-                    continue
-            
-            except():
-                break
-            
-        return os.listdir(artist_folder_path)
-    """    
+    
     def create_txt(self):
         #set up directories to store txt files
         artist_folder_path = "artists/%s" % self.artist
@@ -256,7 +124,6 @@ class Genius_Scraper:
         for i in range(1, 1000):
             j = str(i)            
             
-            #print("ON PAGE " + rts_i)
             try:  
                 data = {"page": i}
                 response = requests.get(artist_songs_url, data=data, headers=headers)
@@ -318,36 +185,13 @@ class Genius_Scraper:
                                         print("TXT FILE ALREADY EXISTS FOR " + song_title)
                             else:
                                 open(blank_path, 'a').close()
-#==============================================================================
-#                                  with open(blank_path, "w") as ifile:
-#                                      try:
-#                                          ifile.write()
-#                                      except UnicodeEncodeError as e:
-#                                          print(e)
-#==============================================================================
-#==============================================================================
-#                                          continue
-#==============================================================================
-                                #print(song_title + " is not in any of the given albums!")
                                 continue
                         except (TypeError, ValueError):
                             #print(self.get_song_title(song_api_path) + " album could not be found!")
                              open(blank_path, 'a').close()                            
                              continue
-#==============================================================================
-#                     else:
-#                         print(self.artist + " is not the primary artist for " + self.get_song_title(song_api_path))   
-#==============================================================================
-            #i = i + 1
                     else:
                         open(blank_path, 'a').close()
-#==============================================================================
-#                         with open(blank_path, "a") as ifile:
-#                             try:
-#                                 ifile.write()
-#                             except UnicodeEncodeError as e:
-#                                 print(e)  
-#==============================================================================
                         continue
             
             except():
